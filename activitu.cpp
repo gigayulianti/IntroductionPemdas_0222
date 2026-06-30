5cab3e37a192328c3022e5f13c8d9b67160f3182
gigayulianti <yulianti2@gmail.com>
2026-06-30 18:27:54 +0700

    coommit 3

--- activitu.cpp
+++ activitu.cpp
@@ -4,23 +4,4 @@ using namespace std;
 int main ()
 {//langkah 1
 
-    int luas, panjang, lebar;
-
-    //langkah 2
-    cout << "Masukkan Panjang :";
-    cin >> panjang;
-
-    //langkah 3
-    cout << "Masukkan lebar :";
-    cin >> lebar;
-
-    //langkah 4
-    luas = panjang * lebar;
-
-    //langkah 5
-    cout << "hasilnya :" << luas;
-
-} //selesai
-
-
-app.cpp2
+    
