e49d6d9a28949b99c392e09acb7d80f4fbb41e7f
gigayulianti <yulianti2@gmail.com>
2026-06-30 18:26:17 +0700

    commit 1

---
+++ activitu.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{//langkah 1
+
+    int luas, panjang, lebar;
+
+    //langkah 2
+    cout << "Masukkan Panjang :";
+    cin >> panjang;
+
+    //langkah 3
+    cout << "Masukkan lebar :";
+    cin >> lebar;
+
+    //langkah 4
+    luas = panjang * lebar;
+
+    //langkah 5
+    cout << "hasilnya :" << luas;
+
+} //selesai
+
+
+app.cpp2
